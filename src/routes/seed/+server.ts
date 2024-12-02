af4b2e282412d0cb7db5f0d04cb45695dc61daf7
Marko Jerkić <markojerkic266@gmail.com>
2024-12-02 19:45:22 +0100

    start trakt

--- src/routes/seed/+server.ts
+++ src/routes/seed/+server.ts
@@ -1,7 +1,64 @@
 import { json } from '@sveltejs/kit';
-import { NY_TIMES_API_KEY } from '$env/static/private';
+import {
+	NY_TIMES_API_KEY,
+	TRAKT_API_CLIENT_ID,
+	TRAKT_API_CLIENT_SECRET
+} from '$env/static/private';
 import { books } from '$lib/server/db';
 
+export async function GET(): Promise<Response> {
+	//await seedBooks();
+	await seedShows();
+
+	return json({ message: 'Books seeded' });
+}
+
+async function seedShows() {
+	const headers = new Headers();
+	headers.append('Content-Type', 'application/json');
+	headers.append('trakt-api-version', '2');
+	headers.append('trakt-api-key', TRAKT_API_CLIENT_ID);
+	const response = await fetch('https://api.trakt.tv/shows/trending');
+
+	console.log(response.ok, response.status, response.statusText, TRAKT_API_CLIENT_ID);
+}
+
+async function seedBooks() {
+	let offset = 0;
+
+	let i = 0;
+	while (i < 1000) {
+		console.log(
+			`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${NY_TIMES_API_KEY}&offset=${offset}`
+		);
+		const response: BookSeedResult = await fetch(
+			`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${NY_TIMES_API_KEY}&offset=${offset}`
+		).then((res) => res.json());
+
+		if (!response.results?.length) {
+			// @ts-expect-error - this is a hack to get around the fact that the API is returning a 200 status code
+			if (response.fault.detail.errorcode === 'policies.ratelimit.QuotaViolation') {
+				console.log('API rate limit reached. Wait a minute and try again.');
+				await new Promise((r) => setTimeout(r, 60000));
+				continue;
+			}
+
+			console.log('no books found', response);
+			break;
+		}
+
+		try {
+			await books.insertMany(response.results);
+		} catch (e) {
+			console.log('error', e);
+		}
+		offset += response.results.length;
+
+		// Rate limiting
+		await new Promise((r) => setTimeout(r, 2000));
+		i++;
+	}
+}
 export interface BookSeedResult {
 	status: string;
 	copyright: string;
@@ -47,47 +104,4 @@ export interface Review {
 	first_chapter_link?: string;
 	sunday_review_link: string;
 	article_chapter_link?: string;
-}
-
-export async function GET(): Promise<Response> {
-	await seedBooks();
-
-	return json({ message: 'Books seeded' });
-}
-
-async function seedBooks() {
-	let offset = 0;
-
-	let i = 0;
-	while (i < 1000) {
-		console.log(
-			`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${NY_TIMES_API_KEY}&offset=${offset}`
-		);
-		const response: BookSeedResult = await fetch(
-			`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${NY_TIMES_API_KEY}&offset=${offset}`
-		).then((res) => res.json());
-
-		if (!response.results?.length) {
-			// @ts-expect-error - this is a hack to get around the fact that the API is returning a 200 status code
-			if (response.fault.detail.errorcode === 'policies.ratelimit.QuotaViolation') {
-				console.log('API rate limit reached. Wait a minute and try again.');
-				await new Promise((r) => setTimeout(r, 60000));
-				continue;
-			}
-
-			console.log('no books found', response);
-			break;
-		}
-
-		try {
-			await books.insertMany(response.results);
-		} catch (e) {
-			console.log('error', e);
-		}
-		offset += response.results.length;
-
-		// Rate limiting
-		await new Promise((r) => setTimeout(r, 2000));
-		i++;
-	}
 }
