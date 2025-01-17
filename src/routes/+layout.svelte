768c9f123d2322333bf48fcaf855f3c46e51d659
Marko Jerkić <markojerkic266@gmail.com>
2025-01-17 19:27:45 +0100

    accessability widget

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>DRUMRE projekt</title>
+	<script src="https://website-widgets.pages.dev/dist/sienna.min.js" defer></script>
+</svelte:head>
+
+{@render children()}
