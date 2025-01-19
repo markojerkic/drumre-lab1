8af7b680fb434f084472efedc3cfa9fbdebf2922
TXort <ort.key@pm.me>
2025-01-19 07:33:17 +0100

    Improve shows page css

--- src/routes/(protected)/shows/+page.svelte
+++ src/routes/(protected)/shows/+page.svelte
@@ -7,38 +7,46 @@
 	let initialSearch = $state(data.search);
 </script>
 
-<h1>Shows</h1>
-
-{#snippet nextPage()}
+{#snippet previousPage()}
 	<div class="paginator">
-		{#if data.cursor && data.cursor > 50}
-			<button
-				class="previous-page"
-				name="cursor"
-				value={Math.max(data.cursor - 100, 0)}
-				form="search-form">Previous page</button
-			>
-		{/if}
-
-		{#if data.hasNext}
-			<button class="next-page" name="cursor" value={data.cursor} form="search-form"
-				>Next page</button
-			>
-		{/if}
+		<button
+			class="previous-page {data.cursor && data.cursor > 50 ? '' : 'hidden'}"
+			name="cursor"
+			value={Math.max(data.cursor - 100, 0)}
+			form="search-form"
+			aria-labelledby="Previous page"
+		>
+		</button>
 	</div>
 {/snippet}
 
-<form method="GET" action="/shows" id="search-form">
-	<label>
-		Search:
-		<input type="text" name="search" bind:value={initialSearch} />
-	</label>
-	<button class="submit-search">Search</button>
+{#snippet nextPage()}
+	<div class="paginator">
+		<button
+			class="next-page {data.hasNext ? '' : 'hidden'}"
+			name="cursor"
+			value={data.cursor}
+			form="search-form"
+			aria-labelledby="Next page"
+		></button>
+	</div>
+{/snippet}
+
+<form method="GET" action="/shows" id="search-form" class="search-form">
+	{@render previousPage()}
+	<div class="search-form">
+		<label>
+			<input type="text" name="search" bind:value={initialSearch} class="search-input"
+						 placeholder="Search shows"
+			/>
+		</label>
+		<button class="submit-search">GO</button>
+		<p class="results">Results: {data.total}</p>
+	</div>
+	{@render nextPage()}
 </form>
 
-{@render nextPage()}
 
-<span class="total">Total shows: {data.total}</span>
 <form method="POST" action="?/delete" use:enhance id="delete"></form>
 <form method="POST" action="?/removeFavourite" use:enhance id="remove-favourite"></form>
 <form method="POST" action="?/addFavourite" use:enhance id="add-favourite"></form>
@@ -49,53 +57,68 @@
 		</ShowThumbnail>
 	{/each}
 </div>
-{@render nextPage()}
+
 
 <style>
-	button.delete {
-		background-color: #dc3545;
-		color: white;
-		padding: 10px;
-		margin-top: 10px;
-	}
-	span.total {
-		font-size: 1.5em;
-	}
+    .paginator {
+        display: flex;
+        justify-content: space-between;
+    }
 
-	.paginator {
-		display: flex;
-		justify-content: space-between;
-	}
+    .next-page, .previous-page, .submit-search {
+        background-color: var(--primary-color);
+        color: var(--text-button-color);
+        padding: 10px;
+        max-width: 200px;
+        max-height: 200px;
+        border-radius: 10px;
+        cursor: pointer;
+        visibility: visible;
+        border: none;
+        font-size: 1.2em;
+    }
 
-	.previous-page {
-		background-color: #dc3545;
-		color: white;
-		padding: 10px;
-	}
+    .next-page:hover, .previous-page:hover, .submit-search:hover {
+        filter: brightness(0.8);
+    }
 
-	.submit-search {
-		background-color: #4caf50;
-		color: white;
-		padding: 10px;
-	}
+    .hidden {
+        visibility: hidden;
+    }
 
-	.next-page {
-		background-color: #007bff;
-		color: white;
-		padding: 10px;
-	}
+    .next-page:after {
+        content: "Next";
 
-	.articles {
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-		gap: 20px;
-	}
-	label {
-		display: block;
-		margin-bottom: 10px;
-	}
-	form > button {
-		margin-top: 10px;
-		margin-bottom: 10px;
-	}
+    }
+
+    .previous-page:after {
+        content: "Previous";
+    }
+
+
+    .search-form {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        padding: 10px;
+        margin: 0 auto;
+    }
+
+    .search-input {
+        padding: 10px;
+        border-radius: 10px;
+        flex-grow: 1;
+        font-size: 1.2em;
+    }
+
+
+    .articles {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        gap: 20px;
+    }
+
+    .results {
+        color: var(--text-color);
+    }
 </style>
